da3d950b8af0a13ed9eb543d6364a18a4399df21
Aline Marques <marques.aline71@yahoo.com>
2025-06-11 18:19:08 -0300

    second commit

--- src/memories.ts
+++ src/memories.ts
@@ -18,55 +18,55 @@ export const memoriesData: Category[] = [
     photos: [
       {
         src: '/images/viagem1.jpeg',
-        caption: 'Nossa primeira viagem juntos! Lembra desse dia na praia?'
+        caption: '❤️'
       },
       {
         src: '/images/viagem2.jpeg',
-        caption: 'Aquele café da manhã com vista para as montanhas. Inesquecível!'
+        caption: '❤️'
       },
       {
         src: '/images/viagem3.jpeg',
-        caption: 'Explorando a cidade grande, perdidos e felizes.'
+        caption: '❤️'
       },
       {
         src: '/images/viagem4.jpeg',
-        caption: 'Aquela vista deslumbrante do alto da montanha.'
+        caption: '❤️'
       },
       {
         src: '/images/viagem5.jpeg',
-        caption: 'Nosso momento relax na praia, com direito a cocada!'
+        caption: '❤️'
       },
       {
         src: '/images/viagem6.jpeg',
-        caption: 'Aquele pôr do sol que nos fez esquecer de tudo.'
+        caption: '❤️'
       },
       {
         src: '/images/viagem7.jpeg',
-        caption: 'Aquela noite estrelada que parecia um sonho.'
+        caption: '❤️'
       },
       {
         src: '/images/viagem8.jpeg',
-        caption: 'Nosso passeio de barco, com direito a música ao vivo.'
+        caption: '❤️'
       },
       {
         src: '/images/viagem9.jpeg',
-        caption: 'Aquela comida de rua que nos conquistou.'
+        caption: '❤️'
       },
       {
         src: '/images/viagem10.jpeg',
-        caption: 'Aquela lembrança que guardamos com carinho.'
+        caption: '❤️'
       },
       {
         src: '/images/viagem11.jpeg',
-        caption: 'Aquela lembrança que guardamos com carinho.'
+        caption: '❤️'
       },
       {
         src: '/images/viagem12.jpeg',
-        caption: 'Aquela lembrança que guardamos com carinho.'
+        caption: '❤️'
       },
       {
         src: '/images/viagem13.jpeg',
-        caption: 'Aquela lembrança que guardamos com carinho.'
+        caption: '❤️'
       },
     ]
   },
@@ -76,43 +76,43 @@ export const memoriesData: Category[] = [
     photos: [
       {
         src: '/images/show1.jpeg',
-        caption: 'Cantando nossa música favorita a plenos pulmões!'
+        caption: '❤️'
       },
       {
         src: '/images/show2.jpeg',
-        caption: 'Aquele festival que quase derretemos de calor, mas valeu cada segundo.'
+        caption: '❤️'
       },
       {
         src: '/images/show3.jpeg',
-        caption: 'Aquele momento mágico quando a banda tocou nossa música.'
+        caption: '❤️'
       },
       {
         src: '/images/show4.jpeg',
-        caption: 'Aquele show que nos fez dançar a noite toda!'
+        caption: '❤️'
       },
       {
         src: '/images/show5.jpeg',
-        caption: 'Aquele show que nos fez dançar a noite toda!'
+        caption: '❤️'
       },
       {
         src: '/images/show6.jpeg',
-        caption: 'Aquele show que nos fez dançar a noite toda!'
+        caption: '❤️'
       },
       {
         src: '/images/show7.jpeg',
-        caption: 'Aquele show que nos fez dançar a noite toda!'
+        caption: '❤️'
       },
       {
         src: '/images/show8.jpeg',
-        caption: 'Aquele show que nos fez dançar a noite toda!'
+        caption: '❤️'
       },
       {
         src: '/images/show9.jpeg',
-        caption: 'Aquele show que nos fez dançar a noite toda!'
+        caption: '❤️'
       },
       {
         src: '/images/show10.jpeg',
-        caption: 'Aquele show que nos fez dançar a noite toda!'
+        caption: '❤️'
       },
     ]
   },
@@ -122,43 +122,43 @@ export const memoriesData: Category[] = [
     photos: [
       {
         src: '/images/casa1.jpeg',
-        caption: 'Sessão de cinema improvisada na sala. Melhor companhia!'
+        caption: '❤️'
       },
       {
         src: '/images/casa2.jpeg',
-        caption: 'Nossa obra de arte na cozinha. Pelo menos a gente se divertiu, né?'
+        caption: '❤️'
       },
       {
         src: '/images/casa3.jpeg',
-        caption: 'Um domingo preguiçoso, que são os melhores.'
+        caption: '❤️'
       },
       {
         src: '/images/casa4.jpeg',
-        caption: 'Sessão de cinema improvisada na sala. Melhor companhia!'
+        caption: '❤️'
       },
       {
         src: '/images/casa5.jpeg',
-        caption: 'Nossa obra de arte na cozinha. Pelo menos a gente se divertiu, né?'
+        caption: '❤️'
       },
       {
         src: '/images/casa6.jpeg',
-        caption: 'Um domingo preguiçoso, que são os melhores.'
+        caption: '❤️'
       },
       {
         src: '/images/casa8.jpeg',
-        caption: 'Nossa obra de arte na cozinha. Pelo menos a gente se divertiu, né?'
+        caption: '❤️'
       },
       {
         src: '/images/casa9.jpeg',
-        caption: 'Um domingo preguiçoso, que são os melhores.'
+        caption: '❤️'
       },
       {
         src: '/images/casa10.jpeg',
-        caption: 'Um domingo preguiçoso, que são os melhores.'
+        caption: '❤️'
       },
       {
         src: '/images/casa11.jpeg',
-        caption: 'Um domingo preguiçoso, que são os melhores.'
+        caption: '❤️'
       },
     ]
   }
